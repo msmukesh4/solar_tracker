c25d1fa527845074f2bd9cbc19146ad4a5fb9d94
Mukesh <msmukesh5>
2017-10-15 18:09:03 +0530

    ui changes in signin and reg pages | sidebar, footer and topbar moved to partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   	protect_from_forgery with: :exception
   	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  	layout :layout_by_resource
+
   	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_number])
 		# devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :invitation_token, :phone_number, :phone_verification_code, :user_role])
@@ -11,5 +13,13 @@ class ApplicationController < ActionController::Base
   	def after_sign_in_path_for(resource_or_scope)
   		user_path(current_user.id)
     end
+
+    def layout_by_resource
+	    if user_signed_in?
+	      'application'
+	    else
+	      'non_logged'
+	    end
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	
 	has_many :devices, :through => :user_devices
 	has_many :user_devices
+
+	def fullname
+    	full_name = ""
+    	if self.first_name && self.last_name
+    		full_name = self.first_name.concat(" ") + self.last_name
+    	elsif self.first_name && !self.last_name
+    		full_name = self.first_name
+    	elsif !self.first_name && self.last_name
+    		full_name = self.last_name
+    	else
+    		full_name = "Guest User"
+    	end
+    	
+    	full_name
+    end
+
 end
